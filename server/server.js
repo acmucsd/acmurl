8fc584338b6320f4497afe2c6b12293c25f29c1d
Jaden Padua <paduajaden@gmail.com>
2019-07-26 00:23:07 -0700

    Set up database

--- server/server.js
+++ server/server.js
@@ -1,31 +1,39 @@
 /**
  * @file Server that runs ACM url shortener
+ * @author Antony Nguyen
+ * @author Jaden Padua
  */
 
 'use strict';
 
-// Dependencies
+// Dependencies.
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const http = require('http');
+const mongoose = require('mongoose');
 
-// Constants
+// Constants.
 const PORT = process.env.PORT || 3000;
 
-// Express Setup
+// Express Setup.
 const app = express();
 const server = http.createServer(app);
+
+// Connect to mongodb.
+const mongoUri = process.env.MONGO_URI ||'mongodb://localhost:27017/stored-urls-db';
+mongoose.connect(mongoUri);
+
 
 app.use(cors({
   credentials: true,
   origin: true,
 }));
 
-// TODO - Connect to database
+// For parsing json data.
+app.use(bodyParser.json());
 
-// TODO - Setup Routes
-
-// Error handling middleware
+// Error handling middleware.
 app.use(function(err, req, res, next) {
   console.error(err);
   res.status(422).send({error: err.message});
