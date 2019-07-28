fa0edf2929e912c6d8980c6ac9d20bd01a6d1e11
casprice <c2price@ucsd.edu>
2019-07-28 14:10:47 -0700

    Add List container

---
+++ client/src/containers/List.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const List = styled.div`
+  margin: 0 auto;
+  width: 60vw;
+
+  @media only screen and (max-width: 800px) {
+    width: 80vw;
+  }
+`;
+
+export default List;
