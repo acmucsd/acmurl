65e7c24089a504bbdfa911163003c07f944357ad
casprice <c2price@ucsd.edu>
2019-08-01 21:03:36 -0700

    Add handleClick function to Button component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,19 +1,28 @@
+import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
   background-color: #22ACEA;
   border: 0;
-  border-radius: 20px;
+  border-radius: 15px;
   color: #FFFFFF;
   cursor: pointer;
   display: inline-block;
-  font-size: 1em;
+  font-family: 'Nunito';
+  font-size: calc(.75em + .5vw);
   margin: 1em;
-  padding: 1em;
+  padding: .5em;
+  width: calc(13em + .5vw);
   
   :focus {
     outline: 0;
   }
 `;
 
-export default Button;
+const handleClick = function() {
+  alert("button clicked!");
+}
+
+export default (props) => (
+  <Button onClick={handleClick}>{props.text}</Button>
+);
