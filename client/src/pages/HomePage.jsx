0a6380976c114b9c494879565ab9de15e96484e5
casprice <c2price@ucsd.edu>
2019-08-01 22:45:24 -0700

    Update ListItem styles

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { GlobalStyle } from '../global-styles';
-import ListItem from '../components/ListItem';
 
 const HomePage = (props) =>  {
   return (
     <div>
       <GlobalStyle/>
-      <ListItem
-        shortURL = "fb"
-        originalURL = "facebook.com/acmucsd"
-        description = "ACM's Facebook Page for announcements on events and projects"
-      />
     </div>
   );
 }
