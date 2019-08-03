2d5069b61926281e2f28ddb664652c441aa2ee5f
eminguyen <emilyhuongnguyen@gmail.com>
2019-08-02 23:51:13 -0700

    Add Input Component

---
+++ client/src/components/Input.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Input = styled.input`
+  background-color: #F4F4F4;
+  border: 0;
+  border-radius: 10px;
+  box-shadow: inset 0px 3px 4px rgba(0, 0, 0, 0.1);
+  color: #333333;
+  font-size: calc(1.75em + .5vw);
+  height: 60px;
+`;
+
+const InputComponent = ({
+  name,
+  type,
+  value,
+  handleChange,
+  placeHolder,
+  ...props
+}) => {
+  return (
+    <Input
+      id={props.name}
+      name={props.name}
+      type={props.type}
+      value={props.value}
+      onChange={props.handleChange}
+      placeholder={props.placeholder}
+    />
+  )
+}
+
+export default InputComponent;
