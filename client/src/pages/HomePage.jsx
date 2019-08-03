f33d1c18e2a01182fe2bb06cdccacd9dfb134677
casprice <c2price@ucsd.edu>
2019-08-03 01:17:37 -0700

    Restore HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,20 +1,11 @@
+    
 import React from 'react';
 import { GlobalStyle } from '../global-styles';
-import List from '../containers/List';
 
 const HomePage = (props) =>  {
-  const object = [{shorturl:'goog', 
-    longurl:'google.com', 
-    description:'a description'}, 
-    {shorturl: 'yah', 
-    longurl:'yahoo.com', 
-    description:'another description'}];
   return (
     <div>
-      <GlobalStyle />
-      <List 
-        urls={object}
-      />
+      <GlobalStyle/>
     </div>
   );
 }
