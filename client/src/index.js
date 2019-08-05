288f391d71b9a67a7c15fbf9734dd9b36ef9d1f4
casprice <c2price@ucsd.edu>
2019-08-04 19:30:38 -0700

    Switch GA to index.js instead of index.html

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import ReactGA from 'react-ga';
 
 import Router from './routes/Router';
 import store from './store';
+
+ReactGA.initialize('UA-145113197-1');
+ReactGA.pageview('/');
 
 ReactDOM.render(
     <Provider store={store}>
