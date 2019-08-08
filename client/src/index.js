dcf677f21ad8f5cc22b99d2a1d711fbabc4f47c2
Casey Price <c2price@ucsd.edu>
2019-08-08 11:54:35 -0700

    Add Google Analytics (Closes #32) (#33)
    Add Google Analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
+import {Provider} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-
+import ReactGA from 'react-ga';
 import Router from './routes/Router';
 import store from './store';
+
+const config = require('../../config.json');
+
+ReactGA.initialize(config.googleAnalytics || process.env.GA);
+ReactGA.pageview('/');
 
 ReactDOM.render(
     <Provider store={store}>
