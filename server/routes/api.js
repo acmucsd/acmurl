acf375aa506efb218121bdd0e35c23e291201aeb
Jaden Padua <paduajaden@gmail.com>
2019-08-12 20:20:26 -0700

    Create api to update existing short url

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,36 +6,46 @@ const router = express.Router();
 const StoredUrl = require('../models/url');
 
 // Post request to add a url to the database.
-router.post('/add-url', function(req, res, next) {
-  StoredUrl.create(req.body).then(function(url) {
+router.post('/add-url', (req, res) => {
+  StoredUrl.create(req.body).then((url) => {
     res.send(url);
     // eslint-disable-next-line no-undef
-  }).catch(function(err) {
+  }).catch((err) => {
     console.log(err);
   });
 });
 
 // Get a list of stored urls from db without internal tag.
-router.get('/get-all-urls', function(req, res, next) {
-  StoredUrl.find({internal: {$exists: false}}).then(function(url) {
+router.get('/get-all-urls', (req, res) => {
+  StoredUrl.find({ internal: { $exists: false } }).then((url) => {
     res.send(url);
   });
 });
 
 // Redirect user to a long url given a short url from the database.
-router.get('/:shorturl', function(req, res, next) {
-  StoredUrl.findOne({shorturl: req.params.id}, {_id: 0, longurl: 1} )
-      .then(function(url) {
-        res.redirect(url.longurl);
-      });
+router.get('/:shorturl', (req, res) => {
+  StoredUrl.findOne({ shorturl: req.params.id }, { _id: 0, longurl: 1 })
+    .then((url) => {
+      res.redirect(url.longurl);
+    });
 });
 
 // Delete an existing url object from the database given a short url.
-router.delete('/:shorturl', function(req, res, next) {
-  StoredUrl.remove({shorturl: req.params.id})
-      .then(function(url) {
-        res.send(url);
-      });
+router.delete('/:shorturl', (req, res) => {
+  StoredUrl.remove({ shorturl: req.params.id })
+    .then((url) => {
+      res.send(url);
+    });
+});
+
+// Update an existing short url from the database given a new short url.
+router.put('/:shorturl', (req, res) => {
+  StoredUrl.findOneAndUpdate(req.params, {
+    $set: {
+      shorturl: req.body.shorturl,
+    },
+  }, { new: true })
+    .then(url => res.send(url.shorturl));
 });
 
 module.exports = router;
